b51b1b845491fb1cc879e09d4c09db3a6418d2c5
mdshimul101 <shimulfjr101@gmail.com>
2022-10-26 00:22:40 +0600

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT ||  5000;
+
+
+app.use(cors());
+
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Dev Skill");
 });
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+
 
 app.listen(port, () =>{
     console.log(`Dev Skill listening on port ${port}`);
